291db33e3171de336c187d79097702a8cf26e1c7
mprevorovsky <mprevorovsky@volny.cz>
2023-09-04 15:50:40 +0200

    added app description

--- src/main/kotlin/com/example/fetch_latest_oots/Main.kt
+++ src/main/kotlin/com/example/fetch_latest_oots/Main.kt
@@ -1,3 +1,11 @@
+// The application should display the latest Order of the Stick (Oots) comic strip image in the browser.
+//
+// There does not seem to be any API available on the Oots server...
+// Therefore, the app at /oots:
+// - reads the Oots comic RSS feed and extracts the URL of the latest strip page
+// - reads the page and extracts the comic image URL, which is then passed to a ThymeLeaf HTML template
+// (note that hotlinking ban needs to be bypassed by using `referrerpolicy="no-referrer"`)
+
 package com.example.fetch_latest_oots
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
